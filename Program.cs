0a2531575cc90cbc830f1ea6324653f5b7c2964c
Petr Martinec <petr.martinec@q2.cz>
2017-02-13 17:18:21 +0100

    upload file with bubble sort

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = {9,2,7,6,4,3,1,8,5};
+
+            printArray(myArray);
+
+            myBubbleSort(myArray);
+
+            printArray(myArray);
+
+            Console.WriteLine();
+        }
+
+        static void printArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        static void myBubbleSort(int[] array)
+        {
+            Boolean swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        int tmp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = tmp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+
+    }
+}
